b5594d3425ff2efb11e5248b6af30c3f619a8b38
Gregory Odiase <Gregory.Odiase@HBNG.COM>
2023-11-27 14:34:51 +0100

    feat: implemented rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [
+    ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([{
+      ttl: 60,
+      limit: 100,
+    }]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
